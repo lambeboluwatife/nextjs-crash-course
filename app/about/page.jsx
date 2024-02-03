c0c924d5157aedabc0f58dc8f44b961cf0cd798a
lambeboluwatife <boluwatifelambe@gmail.com>
2024-02-03 17:08:05 +0100

    set layout and header

---
+++ app/about/page.jsx
@@ -0,0 +1,17 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Traversy Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea explicabo
+        quam aliquam reprehenderit, corporis eveniet consectetur non
+        perspiciatis ut tempore dignissimos sequi accusamus eaque aspernatur
+        labore enim vitae voluptas deleniti magni, quidem temporibus! Vitae ex
+        distinctio aspernatur, assumenda qui, unde deleniti quod nemo suscipit,
+        omnis velit! Voluptas incidunt atque libero!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import { Poppins } from "next/font/google";
+import Header from "./components/Header";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "LBD Media",
+  description: "Web Development tutorials and courses",
+  keywords:
+    "web development, web design, javascript, react, node, html, css, bootstrap, npm, git, github, code, programming, tutorial, course",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <Header />
+        <main className="container">{children}</main>
+      </body>
+    </html>
+  );
+}
